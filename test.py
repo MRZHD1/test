19fbdc0f2a9ee2b11d36ec2e38b825bac5a5ea90
Omer Ali <75752223+MRZHD1@users.noreply.github.com>
2023-10-24 18:21:21 +0000

    test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+def f(n):
+    i = 4
+    while i < n:
+        j = 1
+        print(1)
+        while j < n:
+            j = j * 3
+            print(2)
+
+        k = 0
+        while k < n:
+            k = k + 2
+            print(3)
+        i = i + 1
